897e215456b671db408f1871e56b7130cbc9f362
nomoto <tka303@gmail.com>
2021-01-21 13:04:48 +0900

    1

--- day0121/kaba/js/main.js
+++ day0121/kaba/js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 //onload　全ての文章の読み込みが終わったとき
-window.onload=function(){
+window.onload=()=>{
 	let images=document.getElementsByClassName('headerImage');
 	let zIndexMax=0;
+	let zIndexChange=(eve)=>{eve.target.style.zIndex=++zIndexMax;};
+	let xChange=eve=>{
+	}
 	for(let i=0;i<images.length;i++){
-		images[i].addEventListener("click",function(){
-			this.style.zIndex=++zIndexMax;
-		});
+		images[i].addEventListener("click",zIndexChange);
+		images[i].addEventListener("mouseover",xChange);
 	}
 }
